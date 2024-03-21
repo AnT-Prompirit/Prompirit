daed5d3922f756ce2c708b62b0276abb7763d0c7
Gamddalki <hannahk01@naver.com>
2024-03-21 22:20:49 +0900

    Update Home.tsx and Generating.tsx

--- prompirit/src/Routes/Home.tsx
+++ prompirit/src/Routes/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import bg from "../img/Home.png";
 import Background from "../Components/BackGround";
